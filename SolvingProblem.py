1edbafff2a1a40e1e951f4ceccb017837682b1d1
jjulkakulkaa <jaromirska2002@gmail.com>
2022-12-04 21:27:13 +0100

    FINALL

--- SolvingProblem.py
+++ SolvingProblem.py
@@ -4,16 +4,18 @@ import sys
 from copy import deepcopy
 from math import dist
 from mainGreedy import res_for_GA
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(10**6)
 from Genetic import randomPopulation, calculatedFitness, crossOver, mutatedChromosome, newPopulation, setOfTournaments, tournamentsWinners
-STOP = 0
-def solvingProblem(number_of_points,data,first_population,population_size,best_solutions_amount,iterations,list_of_points):
+
+def solvingProblem(number_of_points,data,first_population,population_size,best_solutions_amount,stop_time,list_of_points, current_time):
     tournaments = setOfTournaments(first_population,population_size)
     winners = tournamentsWinners(data,number_of_points,tournaments)
     newpopulation = newPopulation(number_of_points, data,winners,winners[0:best_solutions_amount], population_size, list_of_points)
     newpopulationcopy = deepcopy(newpopulation)
-    if iterations <= 100:
-        solvingProblem(number_of_points,data, newpopulationcopy, population_size,best_solutions_amount,iterations+1,list_of_points)
+    while time.time() - current_time < stop_time :
+        # print(time.time() - current_time)
+        solvingProblem(number_of_points,data, newpopulationcopy, population_size,best_solutions_amount,stop_time,list_of_points, current_time)
+    
     resutls = []
     for solution in newpopulationcopy:
         resutls.append([solution, calculatedFitness(number_of_points, solution, data)])
