e43cffba89173dfbc7aa73cba0c7307f3f4bec2d
jjulkakulkaa <jaromirska2002@gmail.com>
2022-12-04 19:21:43 +0100

    finall2

--- SolvingProblem.py
+++ SolvingProblem.py
@@ -1,5 +1,4 @@
 import DataReader
-import Genetic
 import time
 import sys
 from copy import deepcopy
@@ -13,7 +12,7 @@ def solvingProblem(number_of_points,data,first_population,population_size,best_s
     winners = tournamentsWinners(data,number_of_points,tournaments)
     newpopulation = newPopulation(number_of_points, data,winners,winners[0:best_solutions_amount], population_size, list_of_points)
     newpopulationcopy = deepcopy(newpopulation)
-    if iterations <= 1000:
+    if iterations <= 100:
         solvingProblem(number_of_points,data, newpopulationcopy, population_size,best_solutions_amount,iterations+1,list_of_points)
     resutls = []
     for solution in newpopulationcopy:
@@ -21,10 +20,3 @@ def solvingProblem(number_of_points,data,first_population,population_size,best_s
     sorted_results = sorted(resutls, key=lambda x:x[1])        
     return sorted_results[0]
 
-size_of_population = 200
-rand_first = randomPopulation(DataReader.list_of_points,size_of_population)
-# rand_first.append(res_for_GA)
-RES = solvingProblem(DataReader.number_of_points,DataReader.data, rand_first ,size_of_population,size_of_population//10,0,DataReader.list_of_points)
-print(RES)
-# print(dist([0,0],[0,3])+ dist([0,3],[0,6])+ dist([0,6],[4,6])+ dist([4,6],[4,4])+dist([4,4],[10,3])+ dist([10,3],[4,2])+ dist([4,2],[4,0])+ dist([4,0],[0,0]))
-#trzeba zrobic wprowadzanie wynikow algorytmu zachlannego do pocztkowej populacji bo sa smieci "rand first"
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import Genetic
-import DataReader
+from DataReader import GetData
+from SolvingProblem import solvingProblem
+from Genetic import randomPopulation, calculatedFitness, crossOver, mutatedChromosome, newPopulation, setOfTournaments, tournamentsWinners
+import time
+
+filename = "berlin52.txt"
+
+read_file = GetData(filename)
+number_of_points = read_file[0]
+list_of_points = list(read_file[1].keys())
+data = read_file[1]
+
+size_of_population = 200
+
+first_population = randomPopulation(list_of_points,size_of_population)
+time0 = time.time()
+result = solvingProblem(number_of_points,data,first_population,size_of_population,size_of_population//10,0,list_of_points)
+print(result, time.time()- time0)
 
